ca51dd9a68acffaa0a8b59d9294ada2bdb108109
rasinoleg <rasinoleg3@gmail.com>
2023-09-02 20:57:22 +0300

    react 5-movies

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'; 
 import { getCastMovie } from 'services/getMovies';
 import { BASE_POSTER_URL, PLACEHOLDER } from 'utils/constants';
 import { ListItem, StyledList } from '../Cast/Cast.module';
 
 const Cast = () => {
   const { movieId } = useParams();
+  const navigate = useNavigate(); 
 
   const [cast, setCast] = useState([]);
+  const [notFound, setNotFound] = useState(false);
 
   useEffect(() => {
     const fetchCast = async () => {
@@ -16,37 +18,41 @@ const Cast = () => {
         setCast(cast);
       } catch (e) {
         console.log(e);
+        setNotFound(true);
       }
     };
     fetchCast();
   }, [movieId]);
+  if (notFound) {
+    navigate('/'); 
+    return null; 
+  }
 
   return (
     <>
-      {
-        <StyledList>
-          {cast.map(({ id, profile_path, original_name, character }) => (
-            <ListItem key={id}>
-              <img
-                src={`${
-                  profile_path
-                    ? BASE_POSTER_URL + profile_path
-                    : PLACEHOLDER + '?text=' + original_name
-                }`}
-                alt={original_name}
-              />
-              <p>
-                <span> Actor:</span> {original_name}
-              </p>
-              <p>
-                <span>Character:</span> {character}
-              </p>
-            </ListItem>
-          ))}
-        </StyledList>
-      }
+      <StyledList>
+        {cast.map(({ id, profile_path, original_name, character }) => (
+          <ListItem key={id}>
+            <img
+              src={`${
+                profile_path
+                  ? BASE_POSTER_URL + profile_path
+                  : PLACEHOLDER + '?text=' + original_name
+              }`}
+              alt={original_name}
+            />
+            <p>
+              <span> Actor:</span> {original_name}
+            </p>
+            <p>
+              <span>Character:</span> {character}
+            </p>
+          </ListItem>
+        ))}
+      </StyledList>
     </>
   );
 };
 
 export default Cast;
+
