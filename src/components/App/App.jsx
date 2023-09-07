605473074c85a3edf4507cc9de774c9d294f2e08
rasinoleg <rasinoleg3@gmail.com>
2023-09-07 23:30:59 +0300

    react 5-movies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,36 @@
+// import { lazy } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import SharedLayout from '../SharedLayout/SharedLayout';
+
+// const Home = lazy(() => import('../../page/Home/Home'));
+// const MoviesDetails = lazy(() =>
+//   import('../../page/MovieDetails/MovieDetails')
+// );
+// const Movies = lazy(() => import('../../page/Movies/Movies'));
+// const Cast = lazy(() => import('../Cast/Cast'));
+// const Reviews = lazy(() => import('../Reviews/Reviews'));
+
+// export const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<SharedLayout />}>
+//         <Route index element={<Home />} />
+//         <Route path="movies" element={<Movies />} />
+//         <Route path="movies/:movieId" element={<MoviesDetails />}>
+//           <Route path="cast" element={<Cast />} />
+//           <Route path="reviews" element={<Reviews />} />
+//           <Route path="about" element={<Navigate to="/about" />} />
+//         </Route>
+//       </Route>
+//     </Routes>
+//   );
+// };
+
+
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import SharedLayout from '../SharedLayout/SharedLayout';
+import About from '../../page/About/About'; 
 
 const Home = lazy(() => import('../../page/Home/Home'));
 const MoviesDetails = lazy(() =>
@@ -19,9 +49,11 @@ export const App = () => {
         <Route path="movies/:movieId" element={<MoviesDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
-          {/* <Route path="about" element={<Navigate to="/about" />} /> */}
+          <Route path="about" element={<About />} /> 
+          <Route path="about" element={<Navigate to="/about" />} />
         </Route>
       </Route>
     </Routes>
   );
 };
+
--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,63 +1,5 @@
-// import React, { useEffect, useState } from 'react';
-// import { useParams, useNavigate } from 'react-router-dom';
-// import { getCastMovie } from 'services/getMovies';
-// import { BASE_POSTER_URL, PLACEHOLDER } from 'utils/constants';
-// import { ListItem, StyledList } from '../Cast/Cast.module';
-
-// const Cast = () => {
-//   const { movieId } = useParams();
-//   const navigate = useNavigate();
-
-//   const [cast, setCast] = useState([]);
-//   const [notFound, setNotFound] = useState(false);
-
-//   useEffect(() => {
-//     const fetchCast = async () => {
-//       try {
-//         const cast = await getCastMovie(movieId);
-//         setCast(cast);
-//       } catch (e) {
-//         console.log(e);
-//         setNotFound(true);
-//       }
-//     };
-//     fetchCast();
-//   }, [movieId]);
-//   if (notFound) {
-//     navigate('/');
-//     return null;
-//   }
-
-//   return (
-//     <>
-//       <StyledList>
-//         {cast.map(({ id, profile_path, original_name, character }) => (
-//           <ListItem key={id}>
-//             <img
-//               src={`${
-//                 profile_path
-//                   ? BASE_POSTER_URL + profile_path
-//                   : PLACEHOLDER + '?text=' + original_name
-//               }`}
-//               alt={original_name}
-//             />
-//             <p>
-//               <span> Actor:</span> {original_name}
-//             </p>
-//             <p>
-//               <span>Character:</span> {character}
-//             </p>
-//           </ListItem>
-//         ))}
-//       </StyledList>
-//     </>
-//   );
-// };
-
-// export default Cast;
-
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { getCastMovie } from 'services/getMovies';
 import { BASE_POSTER_URL, PLACEHOLDER } from 'utils/constants';
 import { ListItem, StyledList } from '../Cast/Cast.module';
@@ -66,24 +8,25 @@ const Cast = () => {
   const { movieId } = useParams();
 
   const [cast, setCast] = useState([]);
-  
+  const [showNoCast, setShowNoCast] = useState(false);
 
   useEffect(() => {
     const fetchCast = async () => {
       try {
         const cast = await getCastMovie(movieId);
         setCast(cast);
+        setShowNoCast(false);
       } catch (e) {
-        console.log(e);
+        console.error(e);
+        setShowNoCast(true);
       }
     };
+
     fetchCast();
   }, [movieId]);
 
-  return (
-    <>
-      {cast.length === 0 ? (
-        <p>No Cast</p>
+  return Cast.length === 0 ? (
+    <h3>No Cast.</h3>
       ) : (
         <StyledList>
           {cast.map(({ id, profile_path, original_name, character }) => (
@@ -105,12 +48,12 @@ const Cast = () => {
             </ListItem>
           ))}
         </StyledList>
-      )}
-    </>
   );
 };
 
 export default Cast;
+
+
 
 
 // function olegСar(objCar) {
